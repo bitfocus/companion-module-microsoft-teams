db53f6259473915168cde45680e4fe6d6c806e13
leonreucher <leonreucher@yahoo.de>
2023-08-27 22:24:37 +0200

    Minor change to the upgrade script

--- upgrade.js
+++ upgrade.js
@@ -2,7 +2,6 @@ export const upgradeScripts = [
     function removeOldInvertFeedbackScripts(context, props) {
         var result;
         for (const feedback of props.feedbacks) {
-            feedback.showInvert = true;
             if (feedback.options['invertFeedback'] !== undefined) {
                 feedback.isInverted = !!feedback.options['invertFeedback']
                 delete feedback.options['invertFeedback']
